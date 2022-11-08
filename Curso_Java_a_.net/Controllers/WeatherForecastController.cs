171656be88f7b18c0098770ea0b43e54090273a4
rgr182 <=>
2022-11-08 12:31:52 -0600

    init boilerplate

---
+++ Curso_Java_a_.net/Controllers/WeatherForecastController.cs
@@ -0,0 +1,24 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Curso_Java_a_.net.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+
+        public readonly ILogger<WeatherForecastController> _logger;
+
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetWeatherForecast")]
+        public string Get()
+        {
+            return "ola k ase";
+        }
+    }
+}
