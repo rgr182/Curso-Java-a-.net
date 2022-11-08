169813c0c14d3be74f1398ba36a2f563b8c6c054
Anand <19041229@itdurango.edu.mx>
2022-11-08 16:47:54 -0600

    Enhancements

--- Curso_Java_a_.net/Controllers/WeatherForecastController.cs
+++ Curso_Java_a_.net/Controllers/WeatherForecastController.cs
@@ -16,9 +16,13 @@ namespace Curso_Java_a_.net.Controllers
         }
 
         [HttpGet(Name = "GetWeatherForecast")]
-        public string Get()
+        public double Get()
         {
-            return "ola k ase";
+            var var1 = 2.56;
+            double var2 = 6.72;
+            var res = var1 + var2;
+
+            return res;
         }
     }
 }
