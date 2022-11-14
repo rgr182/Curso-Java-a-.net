4e27aad5f929d96db4d06e24ed91458a2ee8ff74
rgr182 <=>
2022-11-14 15:57:43 -0600

    Build assemblies fixes

---
+++ Curso_Java_a_.net/Services/IOperaciones.cs
@@ -0,0 +1,11 @@
+﻿namespace Curso_Java_a_.net.Services
+{
+    public interface IOperaciones
+    {
+        public int Suma(int a, int b);
+
+        public int Resta(int a, int b);
+
+        public double Division(int a, int b);       
+    }
+}
---
+++ Curso_Java_a_.net/Services/Operaciones.cs
@@ -0,0 +1,41 @@
+﻿using Curso_Java_a_.net.Controllers;
+using Microsoft.Extensions.Logging;
+using System.ComponentModel.DataAnnotations;
+
+namespace Curso_Java_a_.net.Services
+{
+    public class Operaciones : IOperaciones
+    {
+        ILogger<WeatherForecastController> _logger;
+
+        public Operaciones(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        public int Suma(int a, int b)
+        {
+            int resultado = 0;
+            try
+            {
+                resultado = a + b;
+            }
+            catch (Exception exp)
+            {
+                _logger.LogError("algo trono compadre", exp);
+            }
+
+            return resultado;
+        }
+
+        public int Resta(int a, int b)
+        {
+            return a - b;
+        }
+
+        public double Division(int a, int b)
+        {
+            return a / b;
+        }
+    }
+}
