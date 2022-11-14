c6ed0e85cb68d679dd944834e6d5167bcbfd5297
Anand <19041229@itdurango.edu.mx>
2022-11-13 21:28:54 -0600

    Having trouble with git commands...

---
+++ Curso_Java_a_.net/Calculadora.cs
@@ -0,0 +1,20 @@
+﻿//using Curso_Java_a_.net.Interfaces;
+
+namespace Curso_Java_a_.net.Classes {
+    public class Calculadora:ICalculadora
+    {
+        public Calculadora()
+        {
+        }
+
+        public double Operacion(double Valor1, char Simbolo, double Valor2) { 
+            
+            return Valor1 + Valor2;
+        }
+
+    }
+
+}
+
+
+
---
+++ Curso_Java_a_.net/ICalculadora.cs
@@ -0,0 +1,10 @@
+﻿//namespace Curso_Java_a_.net.Interfaces
+namespace Curso_Java_a_.net.Interfaces {
+    
+    public interface ICalculadora
+    {
+        //double Operacion(double Valor1, char Simbolo, double Valor2);
+    }
+
+}
+
