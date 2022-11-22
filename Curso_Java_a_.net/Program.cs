db8b4e1d5c29ebb6c0144287c5f3e2749e029719
Luis Alberto Jimenez Soto <lajs5257@gmail.com>
2022-11-22 12:36:08 -0600

    added cors

--- Curso_Java_a_.net/Program.cs
+++ Curso_Java_a_.net/Program.cs
@@ -1,10 +1,27 @@
 using Curso_Java_a_.net.DataAccess;
 using Curso_Java_a_.net.Classes;
+using Curso_Java_a_.net.DataAccess.DAL;
 using Microsoft.EntityFrameworkCore;
-using Curso_Java_a_.net.Context.DAL;
-using Curso_Java_a_.net.Data.Access.Services;
+using Curso_Java_a_.Classes;
+using Microsoft.EntityFrameworkCore.Storage.Internal;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
 
 var builder = WebApplication.CreateBuilder(args);
+
+// configuramos el CORS
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+                      builder =>
+                      {
+                          builder.WithOrigins("http://localhost:8080", "http://localhost:3000", "http://127.0.0.1:5500");
+                      });
+});
 
 // Add services to the container.
 
@@ -33,14 +50,16 @@ using (var scope = app.Services.CreateScope())
 }*/
 
 
-    // Configure the HTTP request pipeline.
-    if (app.Environment.IsDevelopment())
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors(MyAllowSpecificOrigins);
 
 app.UseAuthorization();
 
