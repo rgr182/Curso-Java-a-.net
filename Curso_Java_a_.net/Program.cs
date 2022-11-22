180307889785db2faff95f441e312f022c5a8b4c
Luis Alberto Jimenez Soto <lajs5257@gmail.com>
2022-11-22 12:36:08 -0600

    added cors

--- Curso_Java_a_.net/Program.cs
+++ Curso_Java_a_.net/Program.cs
@@ -9,7 +9,19 @@ using System.ComponentModel.DataAnnotations.Schema;
 using Microsoft.EntityFrameworkCore;
 
 
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
 var builder = WebApplication.CreateBuilder(args);
+
+// configuramos el CORS
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+                      builder =>
+                      {
+                          builder.WithOrigins("http://localhost:8080", "http://localhost:3000", "http://127.0.0.1:5500");
+                      });
+});
 
 // Add services to the container.
 
@@ -38,14 +50,16 @@ using (var scope = app.Services.CreateScope())
 }*/
 
 
-    // Configure the HTTP request pipeline.
-    if (app.Environment.IsDevelopment())
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors(MyAllowSpecificOrigins);
 
 app.UseAuthorization();
 
