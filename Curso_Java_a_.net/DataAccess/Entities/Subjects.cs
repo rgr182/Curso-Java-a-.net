e592e3f32b4e82dee4bece1a2c3c28900864e705
Luis Alberto Jimenez Soto <lajs5257@gmail.com>
2022-12-02 16:14:32 -0600

    api boilertemplete

--- Curso_Java_a_.net/DataAccess/Entities/Subjects.cs
+++ Curso_Java_a_.net/DataAccess/Entities/Subjects.cs
@@ -1,7 +1,9 @@
+using System.ComponentModel.DataAnnotations;
 namespace Curso_Java_a_.net.DataAccess.Entities
 {
     public class Subjects
     {
+        [Key]
         public long SubjectId { get; set; }
         public string Name { get; set; }
         public string Schedule { get; set; }
--- Curso_Java_a_.net/DataAccess/Entities/UsersSubjects.cs
+++ Curso_Java_a_.net/DataAccess/Entities/UsersSubjects.cs
@@ -1,7 +1,10 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Curso_Java_a_.net.DataAccess.Entities
 {
     public class UsersSubjects
     {
+        [Key]
         public long SubjectIdUsuario { get; set; }
         public long SubjectId { get; set; }
         public long UserId { get; set; }
