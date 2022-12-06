de5031040e2fb5dcd197881258e788b8832f4465
Daniel M <83463344+Paroxyse@users.noreply.github.com>
2022-12-05 18:28:49 -0600

    -m "Add models for db tables"

---
+++ Curso_Java_a_.net/DataAccess/Entities/Avatar.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Curso_Java_a_.net.DataAccess.Entities
+{
+    public class Avatar
+    {
+        public long Id { get; set; }  
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Type { get; set; }
+        public string Path { get; set; }        
+        public DateTime CreatedAt { get; set; }      
+        public DateTime UpdatedAt { get;set; }
+    }
+}
---
+++ Curso_Java_a_.net/DataAccess/Entities/Contact.cs
@@ -0,0 +1,15 @@
+﻿namespace Curso_Java_a_.net.DataAccess.Entities
+{
+    public class Contact
+    {
+        public long Id { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Country { get; set; }
+        public string PhoneNumber { get; set; }
+        //?????
+        public string UserID { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set;}
+    }
+}
