4c382aec5953cc4827efd973e48d1f8c85a77c9a
Daniel M <83463344+Paroxyse@users.noreply.github.com>
2022-12-12 15:15:27 -0600

    More Mapping

--- Curso_Java_a_.net/DataAccess/Entities/Mapping/BadgesMapping.cs
+++ Curso_Java_a_.net/DataAccess/Entities/Mapping/BadgesMapping.cs
@@ -1,6 +1,38 @@
-﻿namespace Curso_Java_a_.net.DataAccess.Entities.Mapping
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Curso_Java_a_.net.DataAccess.Entities.Mapping
 {
-    public class Class
+    public class BadgesMapping : DbContext
     {
+        protected override void OnModelCreating (ModelBuilder modelBuilder)        
+        {
+            #region Badges
+            modelBuilder.Entity<Badges>()
+                .ToTable("badges");
+            #region Primary Keys
+            modelBuilder.Entity<Badges>()
+                .HasKey(x => x.Id)
+                .HasName("id");
+            #endregion
+            #region Properties
+            modelBuilder.Entity<Badges>()
+                .Property(x => x.Name)
+                .HasColumnName("name")
+                .IsRequired();
+            modelBuilder.Entity<Badges>()
+                .Property(x => x.Description)
+                .HasColumnName("description");
+            modelBuilder.Entity<Badges>()
+                .Property(x => x.Badge)
+                .HasColumnName("badge")
+                .IsRequired();
+            modelBuilder.Entity<Badges>()
+                .Property(x => x.Score)
+                .HasColumnName("score");
+               
+                
+            #endregion
+            #endregion
+        }
     }
 }
