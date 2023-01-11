299c89f8bc9f00fb23698ceb05149f6eb55de0e1
Jeikk <112737017+Jeikk@users.noreply.github.com>
2023-01-10 19:12:36 -0600

    MembersDTO Mapping update

--- Curso_Java_a_.net/DataAccess/DTO/MemberDTO.cs
+++ Curso_Java_a_.net/DataAccess/DTO/MemberDTO.cs
@@ -6,16 +6,12 @@ namespace Curso_Java_a_.net.DataAccess.DTO
         public string Name { get; set; }
         public string FirstName { get; set; }
         public string SecondName { get; set; }
+        public string Email { get; set; }
         public int CurrentLocationId { get; set; }
         public DateTime MemberRegistratior { get; set; }
-        public string Email { get; set; }
-        public string Address { get; set; }
-        public string State { get; set; }
-        public string Zip { get; set; }
-        public string Password { get; set; }
-        public string ConfirmPassword { get; set; }
-        public string PhoneNumber { get; set; }
         public string User { get; set; }
-        public string CV { get; set; }
+        public string Password { get; set; }
+        //public string PhoneNumber { get; set; }
+        //public string CV { get; set; }
     }
 }
