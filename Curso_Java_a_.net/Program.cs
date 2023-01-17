d51209f36ab12594e0c500b58774a5c195583b19
Lajs5257 <82974706+Lajs5257@users.noreply.github.com>
2023-01-17 14:04:55 -0600

    added logger nlog

--- Curso_Java_a_.net/Program.cs
+++ Curso_Java_a_.net/Program.cs
@@ -1,62 +1,86 @@
 using Microsoft.EntityFrameworkCore;
+using NLog;
+using NLog.Web;
 using Curso_Java_a_.net.DataAccess.Repository.Context;
 using Curso_Java_a_.net.Infraestructure;
 using Curso_Java_a_.net.Utils.Security;
 
-var builder = WebApplication.CreateBuilder(args);
+var logger = NLog.LogManager.Setup().LoadConfigurationFromAppSettings().GetCurrentClassLogger();
+logger.Debug("init main");
 
-builder.Services.AddCors(o =>
-    o.AddDefaultPolicy(b =>
-        b.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader()));
-
-// Add services to the container.
-builder.Services.AddControllers();
-
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-
-builder.Services.AddSwaggerGen();
-
-AuthenticationConfig authenticationConfig = new AuthenticationConfig(builder);
-
-builder.Services.AddSingleton<AuthUtils>(new AuthUtils(builder.Configuration));
-
-string connectionStringtest = builder.Configuration.GetConnectionString("EscuelaConnection");
-
-Environment.SetEnvironmentVariable("Connection", connectionStringtest);
-builder.Services.AddDbContext<SchoolSystemContext>(options =>
+try
 {
-    options.UseSqlServer(builder.Configuration.GetConnectionString("EscuelaConnection"));
-});
+    var builder = WebApplication.CreateBuilder(args);
 
-DependencyRegistry registry = new DependencyRegistry(builder);
+    // NLog: Setup NLog for Dependency injection
+    builder.Logging.ClearProviders();
+    builder.Host.UseNLog();
 
-//services cors
-builder.Services.AddCors(p => p.AddPolicy("corsapp", builder =>
+    builder.Services.AddCors(o =>
+        o.AddDefaultPolicy(b =>
+            b.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader()));
+
+    // Add services to the container.
+    builder.Services.AddControllers();
+
+    // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+    builder.Services.AddEndpointsApiExplorer();
+
+    builder.Services.AddSwaggerGen();
+
+    AuthenticationConfig authenticationConfig = new AuthenticationConfig(builder);
+
+    builder.Services.AddSingleton<AuthUtils>(new AuthUtils(builder.Configuration));
+
+    string connectionStringtest = builder.Configuration.GetConnectionString("EscuelaConnection");
+
+    Environment.SetEnvironmentVariable("Connection", connectionStringtest);
+    builder.Services.AddDbContext<SchoolSystemContext>(options =>
+    {
+        options.UseSqlServer(builder.Configuration.GetConnectionString("EscuelaConnection"));
+    });
+
+    DependencyRegistry registry = new DependencyRegistry(builder);
+
+    //services cors
+    builder.Services.AddCors(p => p.AddPolicy("corsapp", builder =>
+    {
+        builder.WithOrigins("*").AllowAnyMethod().AllowAnyHeader();
+    }));
+    var app = builder.Build();
+
+    // Configure the HTTP request pipeline.
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
+
+    app.UseHttpsRedirection();
+
+    app.UseCors();
+
+    app.UseRouting();
+
+    app.UseAuthentication();
+    app.UseAuthorization();
+
+    app.UseEndpoints(endpoints =>
+    {
+        endpoints.MapControllers();
+    });
+
+    app.Run();
+}
+catch (Exception ex)
 {
-    builder.WithOrigins("*").AllowAnyMethod().AllowAnyHeader();
-}));
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    // NLog: catch setup errors
+    logger.Error(ex, "Stopped program because of exception");
+    throw;
 }
 
-app.UseHttpsRedirection();
-
-app.UseCors();
-
-app.UseRouting();
-
-app.UseAuthentication();
-app.UseAuthorization();
-
-app.UseEndpoints(endpoints =>
+finally
 {
-    endpoints.MapControllers();
-});
-
-app.Run();
+    // Ensure to flush and stop internal timers/threads before application exi
+    NLog.LogManager.Shutdown();
+}
